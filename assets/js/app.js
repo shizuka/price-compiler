ef2b6366b9f6d3d2db3425969ca6c95b2363dca8
Jessica Hart <jahart@eganco.com>
2018-06-08 08:33:44 -0500

    moved xlsx logic to angular, added listing

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,8 +11,6 @@ function conlog(msg) {
   console.log("> " + msg);
 }
 
-var bookraws = []; //XLSX format
-var books = []; //just the sheets in foo[row][col] format
 var progbar = document.getElementById('progress');
 function setProgress(lvl, max) {
   progbar.setAttribute('style', 'width: ' + (lvl/max*100) + '%');
@@ -44,116 +42,127 @@ function setProgStatus(msg) { document.getElementById('progdet').textContent = m
     }
   });
 
-  app.controller('PriceUpdateController', function() {
+  app.controller('PriceUpdateController', function($scope) {
+
+    var vm = this;
 
     console.info("Enterprise Price Update Compiler");
     setProgress(0,1);
     setProgStatus("Drag and drop files...");
-    conlog("App loaded.");
 
+    var rABS = true;
+    var reExtension = /(?:\.([^.]+))?$/; //regex to find extension
+    var filestoload = 0;
+    var filesloaded = 0;
+
+    this.books = [];      // [ {filename, format, sheet, ... } , ... ]
+    this.rowsfixed = [];  // [ [desc, date, unit, ...] , ... ]
+    this.hideHowto = false;
+
+    var detectFormat = function (fn, sht) {
+      for (var f = 0; f < priceFormats.length; f++) {
+        var fnmatch = (priceFormats[f].filename.exec(fn) != null);
+        var headmatch = (JSON.stringify(priceFormats[f].headers) == JSON.stringify(sht[0]));
+        if (fnmatch && headmatch) {
+          console.log("...matched " + priceFormats[f].printname);
+          return f;
+        }
+      }
+      return null;
+    }
+
+    var updateLoadProgress = function () {
+      filesloaded++;
+      if (filesloaded < filestoload) {
+        //setProgress(filesloaded,filestoload,true);
+        setProgStatus("Loading files...");
+      } else {
+        setProgStatus("Ready.");
+        setProgress(0,1);
+      }
+    }
+
+    var loadBook = function (f) {
+      var stLoad = new Date();
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        var data = e.target.result;
+        if(!rABS) data = new Uint8Array(data);
+        var workbook = XLSX.read(data, {type: rABS ? 'binary' : 'array', raw: true});
+        var worksheet = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]], {header: 1, blankrows: false, raw: true});
+        for (var i = 0; i < worksheet.length; i++) {
+          for (var j = 0; j < i.length; j++) {
+            if (worksheet[i][j] == null) { worksheet[i][j] = ""; }
+            worksheet[i][j] = worksheet[i][j].toString().replace(/,/g, '');
+          }
+        }
+
+        var format = detectFormat(f.name, worksheet);
+
+        var enLoad = new Date();
+        if (format == null) {
+          conlog("...file [ " + f.name + " ] does not match any known schema, skipped.");
+        } else {
+          var skip = false;
+          for (var i = 0; i < vm.books.length; i++) {
+            if (vm.books[i].format == format) {
+              conlog("...already have a " + priceFormats[format].printname);
+              skip = true;
+            }
+          }
+          if (!skip) {
+            vm.books.push({
+              name: f.name,
+              format: format,
+              formatname: priceFormats[format].printname,
+              sheet: worksheet
+            });
+            $scope.$apply();
+            conlog("Read [ " + f.name + " ] in " + (enLoad - stLoad) + "ms.");
+          }
+        }
+        updateLoadProgress();
+      };
+      if(rABS) reader.readAsBinaryString(f); else reader.readAsArrayBuffer(f);
+    }
+
+    var dropzone = document.getElementById("wrapper");
+
+    var handleDrop = function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+
+      if (e.dataTransfer.items) {
+        filestoload = e.dataTransfer.items.length;
+        filesloaded = 0;
+        setProgStatus("Loading files...");
+        setProgress(1,1);
+        for (var i = 0; i < e.dataTransfer.items.length; i++) {
+          if (e.dataTransfer.items[i].kind === 'file') {
+            var file = e.dataTransfer.items[i].getAsFile();
+            console.log("...["+i+"] " + file.name);
+            var ext = reExtension.exec(file.name)[1];
+            if (ext == "csv" || ext == "xlsx" || ext == "xls") {
+              loadBook(file);
+            } else {
+              filestoload--;
+              console.warn("..["+i+"] has invalid extension " + ext.toUpperCase() + ", skipped.");
+            }
+          }
+        }
+      }
+    }
+    var handleDragover = function (e) {
+      e.preventDefault(); //keep chrome from trying to download files instead of loading
+    }
+
+    dropzone.addEventListener('drop', handleDrop, false);
+    dropzone.addEventListener('dragover', handleDragover, false);
+
+    this.startCompile = function () {
+
+    }
+
+    conlog("App loaded.");
   });
 })();
-
-//**** SHEETJS ****//
-//(function() {
-var rABS = true;
-var reExtension = /(?:\.([^.]+))?$/; //regex to find extension
-var filestoload = 0;
-var filesloaded = 0;
-
-function detectFormat(fn, sht) {
-  for (var f = 0; f < priceFormats.length; f++) {
-    var fnmatch = (priceFormats[f].filename.exec(fn) != null);
-    var headmatch = (JSON.stringify(priceFormats[f].headers) == JSON.stringify(sht[0]));
-    if (fnmatch && headmatch) {
-      console.log("...matched " + priceFormats[f].printname);
-      return f;
-    }
-  }
-  return null;
-}
-
-function updateLoadProgress() {
-  filesloaded++;
-  if (filesloaded < filestoload) {
-    //setProgress(filesloaded,filestoload,true);
-    setProgStatus("Loading files...");
-  } else {
-    setProgStatus("Ready.");
-    setProgress(0,1);
-  }
-}
-
-function loadBook(f) {
-  var stLoad = new Date();
-  var reader = new FileReader();
-  reader.onload = function(e) {
-    var data = e.target.result;
-    if(!rABS) data = new Uint8Array(data);
-    var workbook = XLSX.read(data, {type: rABS ? 'binary' : 'array', raw: true});
-    bookraws.push(workbook); //FOR DEBUGGING LATER
-    var worksheet = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]], {header: 1, blankrows: false, raw: true});
-    for (var i = 0; i < worksheet.length; i++) {
-      for (var j = 0; j < i.length; j++) {
-        if (worksheet[i][j] == null) { worksheet[i][j] = ""; }
-        worksheet[i][j] = worksheet[i][j].toString().replace(/,/g, '');
-      }
-    }
-
-    var format = detectFormat(f.name, worksheet);
-
-    var enLoad = new Date();
-    if (format == null) {
-      conlog("...file [ " + f.name + " ] does not match any known schema, skipped.");
-    } else {
-      for (var book of books) {
-        if (book.format == format) {
-          conlog("...already have a " + priceFormats[format].printname);
-          updateLoadProgress();
-          break;
-        }
-      }
-      books.push({
-        name: f.name,
-        format: format,
-        sheet: worksheet
-      });
-      conlog("...got [ " + f.name + " ][" + priceFormats[format].printname + "] in " + (enLoad - stLoad) + "ms.");
-    }
-    updateLoadProgress();
-  };
-  if(rABS) reader.readAsBinaryString(f); else reader.readAsArrayBuffer(f);
-}
-
-var dropzone = document.getElementById("wrapper");
-function handleDrop(e) {
-  e.preventDefault();
-  e.stopPropagation();
-
-  if (e.dataTransfer.items) {
-    filestoload = e.dataTransfer.items.length;
-    filesloaded = 0;
-    setProgStatus("Loading files...");
-    setProgress(1,1);
-    for (var i = 0; i < e.dataTransfer.items.length; i++) {
-      if (e.dataTransfer.items[i].kind === 'file') {
-        var file = e.dataTransfer.items[i].getAsFile();
-        console.log("...["+i+"] " + file.name);
-        var ext = reExtension.exec(file.name)[1];
-        if (ext == "csv" || ext == "xlsx" || ext == "xls") {
-          loadBook(file);
-        } else {
-          filestoload--;
-          console.warn("..["+i+"] has invalid extension " + ext.toUpperCase() + ", skipped.");
-        }
-      }
-    }
-  }
-}
-function handleDragover(e) {
-  e.preventDefault(); //keep chrome from trying to download files instead of loading
-}
-
-dropzone.addEventListener('drop', handleDrop, false);
-dropzone.addEventListener('dragover', handleDragover, false);
-//})();
